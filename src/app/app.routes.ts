bdde8cc5fbfe31c1a6b5b2a36beda8eb5b19b7eb
Yi <aussina.zhen@gmail.com>
2017-11-20 20:33:34 -0500

    change into production mode
    In main.browser.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { RepoListComponent } from './github/repo-list/repo-list.component';
 import { RepoDetailComponent } from './github/repo-detail/repo-detail.component';
 import { ContactComponent } from './contact/contact.component';
 
+
 export const rootRouterConfig: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
